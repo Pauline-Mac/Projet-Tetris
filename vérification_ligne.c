43c1a37301e5bcb54000c4bfcc47382e9d929e03
clement-le-coadou <104774552+clement-le-coadou@users.noreply.github.com>
2022-05-10 09:06:53 +0200

    Update vérification_ligne.c

--- vérification_ligne.c
+++ vérification_ligne.c
@@ -18,15 +18,44 @@ void eraser(char tab[][TAILLE], int ligne){
 	}
 }
 
+
+void descente_tab(char tab[][TAILLE]){
+	int j=TAILLE-1;
+	while(j>0){
+		for(int i=0;i<TAILLE;i++){
+			tab[j][i]=tab[j-1][i];
+		}
+		j--;
+	}
+}
+
+void affiche(char tab[][TAILLE]){
+	for(int i=0;i<TAILLE;i++){
+		for(int j=0;j<TAILLE;j++){
+			printf("%c|", tab[i][j]);
+		}
+		puts("");
+	}
+}	
+
 int main(){
 	int verif=0;
+	char tab[TAILLE][TAILLE];
+	for(int i=0;i<TAILLE;i++){
+		for(int j=0;j<TAILLE;j++){
+			tab[i][j]='@';
+		}
+		puts("");
+	}
+	affiche(tab);
+	
 	for (int i=0;i<TAILLE;i++){
 		verif=verif_ligne(tab, i);
 		printf("%d\n", verif);
 		if(verif==1)
 			eraser(tab, i);
 	}
+	affiche(tab);
 	
 	return 0;
 }
-
