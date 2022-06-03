973aefb8edfc710af9db96a23882df3fc32c5aed
clement-le-coadou <104774552+clement-le-coadou@users.noreply.github.com>
2022-06-03 18:32:57 +0200

    Update input.h

--- input.h
+++ input.h
@@ -1,3 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+void get_alias(char alias[101], FILE* fp, int* nbline, int* updateScore);
+
+void end_game_score(char alias[101], int* ScorePlayer,FILE* fp, int* updateScore, int* nbline);
