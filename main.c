63388b85f0a250f5d325c97b981c844f07a264b4
Pauline-Mac <104761610+Pauline-Mac@users.noreply.github.com>
2022-06-03 17:38:31 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 #include <time.h>
 #include <unistd.h>
 
+#include <string.h>
+
+
 
 
 const char PIECE_1[4][4][4] = {
@@ -59,6 +62,45 @@ void create_game(char GameGrid[][10]){
 		}
 	}
 }
+
+void get_alias(char alias[101], FILE* fp, int* nbline, int* updateScore){
+	
+
+
+
+	int input = 0;
+	
+	
+	char line[101];
+
+
+
+	while(input==0){
+		puts("Enter alias  ( max 100 ) ");
+		fgets ( alias, 101, stdin );
+		
+		
+		//checks if input is not \n
+		if (strcmp(alias,"\n") != 0){
+			input = 1;
+		}
+	}
+	
+	
+	
+	// check if username already exists
+	while ( fgets(line,101,fp)!=NULL){
+		*(nbline)++;
+		if (strcmp(line,alias) == 0 && *updateScore == 0){
+			puts("WELCOME BACK");
+			*updateScore = 1;
+		}
+		
+	}
+}
+
+
+
 
 int display_rotation(const char tab[][4][4]){
 	int rotat_number=-1;
@@ -269,6 +311,73 @@ int check_line_for_score(char tab[10][10], int* scr){
 	return 0;
 }
 
+void end_game_score(char alias[101], int* ScorePlayer,FILE* fp, int* updateScore, int* nbline){
+	
+	
+	char line[101];
+	char oldScoreTab[101];
+	int oldScore;
+	char username[100];
+	char usernameScoreTab[100];
+	int usernameScore = 0;
+	int bestscore = 0;
+	char BestScoreUsername[100];
+	
+	
+	// at end of game
+	if (*nbline == 1){
+		fseek(fp, 0, SEEK_SET);
+	}
+	if (*updateScore !=1){
+		fprintf(fp, "%s", alias);
+		fprintf(fp, "%d\n", *ScorePlayer);
+	}
+	else{
+		fseek(fp, 0, SEEK_SET);
+		while ( fgets(line,101,fp)!=NULL){
+			
+			if (strcmp(line,alias) == 0){
+				
+				fgets(oldScoreTab,101,fp);
+				oldScore = atoi(oldScoreTab);
+				
+				if ( *ScorePlayer > oldScore){
+					fseek(fp, -strlen(oldScoreTab), SEEK_CUR);
+					fprintf(fp, "%d\n", *ScorePlayer);
+				}
+			}
+		}
+		
+	}
+	
+	rewind(fp);
+	
+	// check if first player
+	if (*nbline == 1){
+		printf("you are the first player, new best score is: %d",*ScorePlayer);
+	}
+	//if not first player checks for best score inside data base
+	
+	
+	
+	while ( fgets(username,101,fp)!=NULL && fgets(usernameScoreTab,101,fp)!=NULL){
+		usernameScore = atoi(usernameScoreTab);
+		printf("player: %s your score is : %d\n", username, usernameScore);
+		// saves best score and best score username
+		if ( usernameScore >= bestscore){
+			bestscore = usernameScore;
+			strcpy(BestScoreUsername, username);
+		}
+	}
+		
+	printf("The best score is: %d scored by %s\n", bestscore,BestScoreUsername);
+	
+	fclose(fp);
+	sleep(3);
+	exit(0);
+}
+
+
 void end_game(){
 	system("clear");
 	puts("                 Your block exceeded the grid");
@@ -280,7 +389,6 @@ void end_game(){
 	puts("   | |__| |/ ____ \\| |  | | |____  | |__| | \\  /  | |____| | \\ \\");
 	puts("    \\_____/_/    \\_\\_|  |_|______|  \\____/   \\/   |______|_|  \\_\\");
 	sleep(3);
-	exit(0);
 	
 }
 
@@ -288,6 +396,21 @@ void end_game(){
 
 int main(int argc, char **argv)
 {
+	char name[101];
+	FILE* file=NULL;
+	file=fopen("score_data.txt", "a+");
+	int score = 0;
+	int* pscore = &score;
+	int updateScore = 0;
+	int* pupdateScore = &updateScore;
+	int nbline = 0;
+	int* pnbline = &nbline;
+	
+	
+	get_alias(name,file, pnbline,pupdateScore);
+	
+	
+	
 	char game[10][10];
 	int playing=0;
 	int verif=0;
@@ -296,7 +419,6 @@ int main(int argc, char **argv)
 	int tetrimimo;
 	int rotat_number_piece;
 	
-	int score=0;
 	
 	
 	puts("------------------WELCOME-------------------------");
@@ -337,6 +459,7 @@ int main(int argc, char **argv)
 				}
 				else {
 					end_game();
+					end_game_score(name,pscore,file, pupdateScore, pnbline);
 					playing = 0;
 				}
 				break;
@@ -352,6 +475,7 @@ int main(int argc, char **argv)
 				}
 				else {
 					end_game();
+					end_game_score(name,pscore,file, pupdateScore, pnbline);
 					playing = 0;
 				}
 				break;
@@ -367,6 +491,7 @@ int main(int argc, char **argv)
 				}
 				else {
 					end_game();
+					end_game_score(name,pscore,file, pupdateScore, pnbline);
 					playing = 0;
 				}
 				break;
@@ -382,6 +507,7 @@ int main(int argc, char **argv)
 				}
 				else {
 					end_game();
+					end_game_score(name,pscore,file, pupdateScore, pnbline);
 					playing = 0;
 				}
 				break;
@@ -397,6 +523,7 @@ int main(int argc, char **argv)
 				}
 				else {
 					end_game();
+					end_game_score(name,pscore,file, pupdateScore, pnbline);
 					playing = 0;
 				}
 				break;
@@ -412,6 +539,7 @@ int main(int argc, char **argv)
 				}
 				else {
 					end_game();
+					end_game_score(name,pscore,file, pupdateScore, pnbline);
 					playing = 0;
 				}
 				break;
@@ -427,6 +555,7 @@ int main(int argc, char **argv)
 				}
 				else {
 					end_game();
+					end_game_score(name,pscore,file, pupdateScore, pnbline);
 					playing = 0;
 				}
 				break;
