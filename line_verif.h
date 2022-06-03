f552968cb84ecb64f12fc6a943c889ec3667afc2
clement-le-coadou <104774552+clement-le-coadou@users.noreply.github.com>
2022-06-03 18:34:01 +0200

    Update line_verif.h

--- line_verif.h
+++ line_verif.h
@@ -1,1 +1,9 @@
 #include <stdio.h>
+
+int verif_line (char tab[][10], int line);
+
+void eraser(char tab[][10], int line);
+
+void table_descent (char tab[][10], int line);
+
+int check_line_for_score(char tab[10][10], int* scr);
