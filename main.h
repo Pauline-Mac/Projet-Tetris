6bf3b14d15568c0c307bf28f6362c2294e230bbf
Pauline-Mac <104761610+Pauline-Mac@users.noreply.github.com>
2022-06-03 19:11:52 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,5 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
-#include <string.h>
+#ifndef LYBRARY
+  #define LYBRARY
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <time.h>
+  #include <unistd.h>
+  #include <string.h>
+#endif
