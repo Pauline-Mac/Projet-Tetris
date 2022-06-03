de32812a53cf05115583ec01a13d3a4513ccc1d8
clement-le-coadou <104774552+clement-le-coadou@users.noreply.github.com>
2022-06-03 18:39:10 +0200

    Update name_manager.h

--- name_manager.h
+++ name_manager.h
@@ -2,3 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+void get_alias(char alias[101], FILE* fp, int* nbline, int* updateScore);
+
+void end_game_score(char alias[101], int* ScorePlayer,FILE* fp, int* updateScore, int* nbline);
