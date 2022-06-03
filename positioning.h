c7e83023a56e9eed303c25b77c45edf06c445a4a
clement-le-coadou <104774552+clement-le-coadou@users.noreply.github.com>
2022-06-03 18:41:00 +0200

    Update positioning.h

--- positioning.h
+++ positioning.h
@@ -2,3 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+
+void create_game(char GameGrid[][10]);
+
+int check_room(char game[10][10], int column, int line, const char tetrimo[4][4]);
+
+int get_lowest_line(char game[10][10], int column, const char tetrimo[4][4], int random);
+
+int place_block(const char tetrimimo[4][4], char game[10][10])
