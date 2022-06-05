15366f1265144387fc667d0f7d9674cd4018a594
Pauline-Mac <104761610+Pauline-Mac@users.noreply.github.com>
2022-06-05 20:07:26 +0200

    commentaire

--- name_manager.c
+++ name_manager.c
@@ -1,13 +1,10 @@
 #include "name_manager.h"
 
 
+// ask for player name and check if already in data base
 void get_alias(char alias[101], FILE* fp, int* nbline, int* updateScore){
 	
-
-
-
 	int input = 0;
-	
 	
 	char line[101];
 
@@ -37,7 +34,7 @@ void get_alias(char alias[101], FILE* fp, int* nbline, int* updateScore){
 	}
 }
 
-
+// show best score and add final score to data base
 void end_game_score(char alias[101], int* ScorePlayer,FILE* fp, int* updateScore, int* nbline){
 	
 	
@@ -51,14 +48,18 @@ void end_game_score(char alias[101], int* ScorePlayer,FILE* fp, int* updateScore
 	char BestScoreUsername[100];
 	
 	
+	/* UPDATE SCORE OR ADD PLAYER NAME AND FINAL SCORE TO DATA BASE*/
+	
 	// at end of game
 	if (*nbline == 1){
 		fseek(fp, 0, SEEK_SET);
 	}
+	
 	if (*updateScore !=1){
 		fprintf(fp, "%s", alias);
 		fprintf(fp, "%d\n", *ScorePlayer);
 	}
+	// update old score if inferior to new score
 	else{
 		fseek(fp, 0, SEEK_SET);
 		while ( fgets(line,101,fp)!=NULL){
@@ -79,13 +80,13 @@ void end_game_score(char alias[101], int* ScorePlayer,FILE* fp, int* updateScore
 	
 	rewind(fp);
 	
+	/* SHOW BEST SCORE*/
+	
 	// check if first player
 	if (*nbline == 1){
 		printf("you are the first player, new best score is: %d",*ScorePlayer);
 	}
 	//if not first player checks for best score inside data base
-	
-	
 	
 	while ( fgets(username,101,fp)!=NULL && fgets(usernameScoreTab,101,fp)!=NULL){
 		usernameScore = atoi(usernameScoreTab);
